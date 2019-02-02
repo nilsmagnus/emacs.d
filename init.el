5e75f42c3cd14032f41b65e8d0697db3b592f934
Nils Larsgård <nilsmagnus@gmail.com>
2019-02-02 08:41:59 +0100

    Add readme and markdown-mode

--- init.el
+++ init.el
@@ -27,3 +27,17 @@
 (require 'go-autocomplete)
 (require 'auto-complete-config)
 (ac-config-default)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (markdown-mode exec-path-from-shell go-imports go-eldoc elm-mode dockerfile-mode company-go auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
