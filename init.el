62890112e79e609bbd09bbf082722667aa0079ff
Nils Larsgård <nilsmagnus@gmail.com>
2019-02-02 21:54:15 +0100

    More stuff and comments

--- init.el
+++ init.el
@@ -1,12 +1,31 @@
 ;; load emacs 24's package system. Add MELPA repository.
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+;; add go-autocomplete and stuff related to go-config
+(add-to-list 'load-path "~/.emacs.d/gomacs/")
+
+;; get path that is used from shell
 (exec-path-from-shell-initialize)
+
+;; enable flycheck for everything
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+
+;; custom flycheck for golang
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-golangci-lint-setup))
+
+;; function to format go-code
+(defun go-save-hook ()
+  (when (eq major-mode 'go-mode)
+    (gofmt)))
+
+;; format go-code on save
+(add-hook 'before-save-hook #'go-save-hook)
 
 (when (>= emacs-major-version 24)
   (require 'package)
@@ -16,6 +35,7 @@
    '("melpa" . "http://melpa.milkbox.net/packages/")
    t))
 
+;; add go-imports to gofmt command
 (setq gofmt-command "goimports")
 
 (defun my-go-mode-hook()
@@ -24,10 +44,11 @@
 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 
-(add-to-list 'load-path "~/.emacs.d/gomacs/")
 
+;; autocomplete for golang
 (require 'go-autocomplete)
 (require 'auto-complete-config)
+
 (ac-config-default)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -36,7 +57,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (markdown-mode exec-path-from-shell go-imports go-eldoc elm-mode dockerfile-mode company-go auto-complete))))
+    (flycheck-golangci-lint markdown-mode exec-path-from-shell go-imports go-eldoc elm-mode dockerfile-mode company-go auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
