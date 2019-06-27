7d7b48b7873a88797b2dc6fe1ea51f641baef22d
Nils Larsgård <nilsmagnus@gmail.com>
2019-06-27 07:38:38 +0200

    Auto-install packages, refactor some stuff, remove some stuff

--- init.el
+++ init.el
@@ -1,51 +1,52 @@
+(setq exec-path (append exec-path '("~/go/bin")))
+	       
+(setq inhibit-startup-screen t)
+(setq package-list '(go-guru go-mode markdown-mode exec-path-from-shell go-imports go-eldoc dockerfile-mode exec-path-from-shell company company-go))
+
+(require 'package)
+(add-to-list
+   'package-archives
+   ;;'("melpa" . "http://melpa.org/packages/") ; many packages won't show if using stable
+   ;;'("melpa" . "http://melpa.milkbox.net/packages/")
+   '("marmalade" . "http://marmalade-repo.org/packages/")
+   t)
+
+; activate all the packages (in particular autoloads)
 (package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
 
 ;; backup config, tips from https://stackoverflow.com/questions/151945/how-do-i-control-how-emacs-makes-backup-files
 (setq backup-directory-alist `(("." . "~/.saves")))
-(setq backup-by-copying t)
 (setq delete-old-versions t
   kept-new-versions 6
   kept-old-versions 2
   version-control t)
 
+;; GO - stuff
 
-;; enable flycheck for everything
-(add-hook 'after-init-hook #'global-flycheck-mode)
 
-;; company-mode for everyone
-(add-hook 'after-init-hook 'global-company-mode)
-
-;; elm stuff
-
-(with-eval-after-load 'flycheck
-    '(add-hook 'flycheck-mode-hook #'flycheck-elm-setup))
-
-(with-eval-after-load 'company-mode
-  '(add-to-list 'company-backends 'company-elm))
-
-(defun elm-save-hook ()
-  (when (eq major-mode 'elm-mode)
-    '((elm-format)
-       )))
-
-(add-hook 'after-save-hook #'elm-save-hook)
-
-(setq elm-tags-on-save t)
-
-(add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
-
-;; --- elm stuff end
-
-;; --- golang stuff
 (require 'go-guru)
+
+;; autocompletion stuff
+(add-to-list 'load-path "~/.emacs.d/autocomplete/")
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
+
+;;(require 'go-repl-mode)
+
+(setq gofmt-command "goimports")
+
 (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
 
-;;  go-autocomplete and stuff related to go-config
-(add-to-list 'load-path "~/.emacs.d/gomacs/")
-
-;; custom flycheck for golang
-(eval-after-load 'flycheck
-  '(add-hook 'flycheck-mode-hook #'flycheck-golangci-lint-setup))
 
 
 ;; function to format go-code
@@ -56,15 +57,6 @@
 ;; format go-code on save
 (add-hook 'before-save-hook #'go-save-hook)
 
-(add-hook 'go-mode-hook #'gorepl-mode)
-
-(require 'package)
-
-(setq package-archives '(
-		         ("melpa" . "https://melpa.org/packages/")))
-
-;; add go-imports to gofmt command
-(setq gofmt-command "goimports")
 
 (defun my-go-mode-hook()
   (local-set-key (kbd "C-c m") 'gofmt)
@@ -73,14 +65,8 @@
 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 
+;; END GO 
 
-;; autocomplete for golang
-					;
-(require 'go-autocomplete)
-(require 'auto-complete-config)
-(require 'gorepl-mode)
-
-(ac-config-default)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -88,7 +74,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (flycheck go-guru gorepl-mode ## flycheck-elm company go-mode flycheck-gometalinter flycheck-golangci-lint markdown-mode exec-path-from-shell go-imports go-eldoc elm-mode dockerfile-mode company-go auto-complete))))
+    (auto-complete-confi auto-complete-config go-autocomplete go-guru))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
